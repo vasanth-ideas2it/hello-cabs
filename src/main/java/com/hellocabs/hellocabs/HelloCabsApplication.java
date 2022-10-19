343b64f0c45f018acef479a037f8175e840db26d
pradeepi2i <pradeeparulsamy@ideas2it.com>
2022-10-19 10:20:36 +0530

    Initial commit for cab service

---
+++ src/main/java/com/hellocabs/hellocabs/HelloCabsApplication.java
@@ -0,0 +1,13 @@
+package com.hellocabs.hellocabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellocabs/hellocabs/HelloCabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellocabs.hellocabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
